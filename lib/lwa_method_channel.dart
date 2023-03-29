9b7e1715c825961ef77cd60a65b44d24863b6e45
Greg Gushard <greggushard@icloud.com>
2023-03-29 12:58:42 -0400

    added dartdoc comments

--- lib/lwa_method_channel.dart
+++ lib/lwa_method_channel.dart
@@ -3,22 +3,33 @@ import 'package:flutter/services.dart';
 
 import 'lwa_platform_interface.dart';
 
+/// MethodChannelLwa
 class MethodChannelLwa extends LwaPlatform {
   @visibleForTesting
   final methodChannel = const MethodChannel('lwa');
 
   @override
+  /// signIn(scopes)
+  /// Logs the user in to the Amazon service
+  /// Accepts an optional array of scopes and returns a string
   Future<String?> signIn({required List scopes}) async {
-    final response = await methodChannel.invokeMethod<String>('signIn', {'scopes': scopes});
+    final response =
+        await methodChannel.invokeMethod<String>('signIn', {'scopes': scopes});
     return response;
   }
 
   @override
+  /// signOut()
+  /// Logs the user out of the Amazon service and expires their token
+  /// Returns a string
   Future<String?> signOut() async {
     final response = await methodChannel.invokeMethod<String>('signOut');
     return response;
   }
 
+  /// getLWAAuthState
+  /// Stream handler that listens for Amazon authentication events
+  /// Returns a stream
   static Stream get getLWAAuthState {
     const eventChannel = EventChannel('lwa.authentication');
     return eventChannel.receiveBroadcastStream().cast();
